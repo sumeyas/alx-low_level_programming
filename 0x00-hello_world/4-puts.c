28fc7c0dbf6ae084f2e3b52e9b50cfa79958b5ed
sumeya <sumeyaskr@gmail.com>
2022-06-30 07:45:03 -0700

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,23 +2,17 @@
 #include<string.h>
 
 /**
- *  I hate betty coding style 
+ * This program is printing a string
  */
 
 /**
- * prints string
+ * Main description
  */
 int main(void)
 {
 	char p[52];
 
-	/**
-	 * here it prints
-	 */
 	strcpy(p, "\"Programming is like building a multilingual puzzle");
-	puts(p);
-	/**
-	 * print
-	 */
+	puts(p)
 	return (0);
 }
