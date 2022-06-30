8d5d86960b68b9eee4a55cc8d3358a00e89b8807
sumeya <sumeyaskr@gmail.com>
2022-06-30 11:14:37 -0700

    7

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: ", sizeof(char), "byte(s)");
+	printf("Size of an int: ", sizeof(int), "bytes(s)");
+	printf("Size of a long int: ", sizeof(long), "bytes(s)");
+	printf("Size of a long long int: " sizeof(long long), "bytes(s)");
+	printf("Size of a float: " sizeof(float), "byte(s)");
+}
