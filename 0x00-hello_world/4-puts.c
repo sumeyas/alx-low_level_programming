8fa02aaa4ef80099dc3261ec0c18ef0511b28f5f
sumeya <sumeyaskr@gmail.com>
2022-06-30 07:05:21 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 
+/**
+ * I hate betty styling */
+
 int main(void)
 {
-	char p[53];
-	strcpy (p, "\"Programming is like building a multilingual puzzle\n");
+	char p[52];
+
+	strcpy(p,"\"Programming is like building a multilingual puzzle\n");
 	puts(p);
-	return(0);
+	return (0);
 }
