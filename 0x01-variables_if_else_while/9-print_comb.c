a815f16d183957aae944024d5594620ad89a776f
sumeya <sumeyaskr@gmail.com>
2022-07-02 08:07:58 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,9 +4,10 @@ int main(void)
 {
 	int i = 0;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		putchar(i, ' ');
+		putchar(i);
+		putchar(', ');
 	}
 	putchar('\n');
 	return (0);
