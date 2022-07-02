d3a512c70645b1b87d01838da59b8bab9b820cbd
sumeya <sumeyaskr@gmail.com>
2022-07-02 07:30:52 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int ch;
+	int num;
+
+	for (ch = 0; ch <=9; ch++)
+	{
+		putchar(ch);
+	}
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
