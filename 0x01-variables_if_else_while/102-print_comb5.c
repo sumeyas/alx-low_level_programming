95d01eb8681b16265cfa1c0826b7b38b4b053b2d
sumeya <sumeyaskr@gmail.com>
2022-07-04 12:29:43 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,31 @@
 int main(void)
 {
 	int c = 0;
+	int k = 0;
 	int i = 0;
 	int j = 0;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		for (k = '0'; k <= '9'; k++)
 		{
-			for (j = '0'; j <= '9'; j++)
+			for (i = '0'; i <= '9'; i++)
 			{
-				putchar(c);
-				putchar(i);
-				putchar(' ');
-				putchar(i);
-				putchar(j);
-				if (!(c == '9' && i == '8'))
+				for (j = '0'; j <= '9'; j++)
 				{
-					putchar(',');
+					putchar(c);
+					putchar(k);
 					putchar(' ');
+					putchar(i);
+					putchar(j);
+					if (!(c == '9' && k == '8' && i == '9' j == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-			}
-		}
-	}
+			}	
+	}	}
 	putchar('\n');
 	return (0);
 }
