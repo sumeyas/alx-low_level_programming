6af36a4f25343e3c0f6585886e515ab7f9bba6fc
sumeya <sumeyaskr@gmail.com>
2022-07-04 11:41:13 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,17 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '9';j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			if (i < j){
+			if (i < j)
+			{
 				putchar(i);
 				putchar(j);
-				putchar(',');
-				putchar(' ');
+				if (i != '8' && j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
