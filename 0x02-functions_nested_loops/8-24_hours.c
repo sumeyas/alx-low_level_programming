3c2082cee20277b2300169577faac2afe8e383ab
sumeya <sumeyaskr@gmail.com>
2022-07-06 08:40:10 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,21 +12,26 @@ void jack_bauer(void)
 	{
 		for (m = '0'; m <= '9'; m++)
 		{
-			for (i = '0'; i < '6'; i++)
-				{
-					for (j = '0'; j <= '9'; j++)
+			if(!( h == 2 && m == 4))
+			{
+				
+				for (i = '0'; i < '6'; i++)
+					{
+						for (j = '0'; j <= '9'; j++)
 						{
-							if (!(h == 2 && m == 4))
-							{
 								_putchar(h);
 								_putchar(m);
 								_putchar(':');
 								_putchar(i);
 								_putchar(j);
 								_putchar('\n');
-							}
 						}
-				}
+					}
+			}
+			else
+			{
+				break;
+			}
 		}
 	}
 }
