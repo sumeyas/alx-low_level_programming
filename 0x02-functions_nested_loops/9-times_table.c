13cfff4ef02f283466e77cb4e99a04e13d6e7697
sumeya <sumeyaskr@gmail.com>
2022-07-06 09:23:45 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 	int j;
 	int ch;
 
-	for (i == '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j == '0'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			ch = i * j;
 			_putchar(ch);
