cbe545a86af9831159a63e2b3ce9fc3d1789e60c
sumeya <sumeyaskr@gmail.com>
2022-07-06 00:08:03 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main- description
+ *
+ * Return: value
+ */
+int main (void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,12 @@
 #include<stdio.h>
 
 int  _putchar(int ch);
+void print_alphabet()
+{
+	char ch = 'a'
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+}
