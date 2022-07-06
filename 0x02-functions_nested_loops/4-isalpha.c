12d4dc782cc5d18b7ebc8198fc553e11085c4eb2
sumeya <sumeyaskr@gmail.com>
2022-07-06 01:37:52 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - description
+ *
+ * @c: the parameter
+ *
+ * Return: value
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <='Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
