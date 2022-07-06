eb177c360feb3dc6fd892a89971f10ce7a3e1127
sumeya <sumeyaskr@gmail.com>
2022-07-06 11:49:15 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int sum = 0;
+	int n = 0;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum = n + sum;
+		}
+		n++;
+	}
+	putchar('\n');
+}
