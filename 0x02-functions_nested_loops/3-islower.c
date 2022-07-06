64bff962fd823ef2795a6a6d405c45ea0dad0c2e
sumeya <sumeyaskr@gmail.com>
2022-07-06 01:24:29 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _islower - description
+ *
+ * Return: integer
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <='z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
