2124c35ed23cc46bcc4d129633be9279f30773c2
sumeya <sumeyaskr@gmail.com>
2022-07-06 05:34:41 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 6; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i);
+			_putchar(j);
+			_putchar('\n');
+		}
+	}
+
