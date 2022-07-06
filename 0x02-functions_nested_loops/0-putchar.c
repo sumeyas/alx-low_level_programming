058a65af41517a99419c0c84f32b705c240066a4
sumeya <sumeyaskr@gmail.com>
2022-07-06 00:00:46 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,22 @@
 #include "main.h"
 
+/**
+ * main - description
+ *
+ * Return: value
+ */
 int main(void)
 {
-	int ch = "_putchar";
 
-	_putchar(ch);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
