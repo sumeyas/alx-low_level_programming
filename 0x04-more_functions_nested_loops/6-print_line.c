f8038c803054430fff9ee02e746c2f3ea11a1115
sumeya <sumeyaskr@gmail.com>
2022-07-07 07:57:41 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,18 @@
  */
 void print_line(int n)
 {
+	int i = 0;
+
 	if (n > 0)
 	{
-		while (n > 0)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
-			n = n - 1;
 		}
 		_putchar('\n');
 	}
 	else
 	{
 		_putchar('\n');
+	}
 }
