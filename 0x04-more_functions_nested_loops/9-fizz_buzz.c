efe7fae9472e80c790012f10a7b6ae28fa5872d0
sumeya <sumeyaskr@gmail.com>
2022-07-07 12:00:08 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,13 +4,13 @@
  * main - description
  * Return: value
  */
-int main (void)
+int main(void)
 {
 	int i = 1;
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("Fizz");
 			putchar(' ');
@@ -20,10 +20,11 @@ int main (void)
 			printf("Buzz");
 			putchar(' ');
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 3 == 0)
 		{
 			printf("FizzBuzz");
 			putchar(' ');
+		}
 		else
 		{
 			printf("%d", i);
