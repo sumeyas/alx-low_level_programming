b63a97ff59a5afd375f2480e7f1ab9c0c937c693
sumeya <sumeyaskr@gmail.com>
2022-07-07 07:05:07 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - description
- * 
+ * print_numbers - desc
  */
-
 void print_numbers(void)
 {
-	int ch;
+	int n;
+	char ch;
 
-	for (ch = '0'; ch <= '9'; ch++)
+	for (n = 0; n < 10; n++)
 	{
+		ch = n + '0';
 		_putchar(ch);
 	}
 	_putchar('\n');
