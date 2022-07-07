77da716e35559cf959f5fb6984efc904d46ab355
sumeya <sumeyaskr@gmail.com>
2022-07-07 00:21:43 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)/*i is not incrementing*/
+	while (i < 10)
 	{
-		putchar(i);
+		putchar(i);/*i is not incrementing*/
 	}
 
 	printf("infinite loop avoided! \\o/\n");
