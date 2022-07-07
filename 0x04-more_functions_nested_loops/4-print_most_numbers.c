9a4ad162cb2ca7ee45fafa062002b713ccba2fea
sumeya <sumeyaskr@gmail.com>
2022-07-07 07:00:31 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,13 @@ void print_most_numbers(void)
 	int n;
 	char ch;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
-		if (n != 2 || n != 4)
+		if (!(n == 2 || n == 4))
 		{
 			ch = n + '0';
 			_putchar(ch);
 		}
 	}
 	_putchar('\n');
-}	
+}
