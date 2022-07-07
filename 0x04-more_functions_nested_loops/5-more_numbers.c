2cc99c8e18810676006a619c8e4f68d86e6b26df
sumeya <sumeyaskr@gmail.com>
2022-07-07 09:23:20 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,26 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int k;
+	int n;
 	char ch;
 
 	for (i = 0; i < 11; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			ch = j + '0';
-			_putchar(ch);
+			if (j >= 10)
+			{
+				n = j / 10 + '0';
+				_putchar(n);
+				k = j % 10 + '0';
+				_putchar(k);
+			}
+			else
+			{
+				ch = j + '0';
+				_putchar(ch);
+			}
 		}
 		_putchar('\n');
 	}
