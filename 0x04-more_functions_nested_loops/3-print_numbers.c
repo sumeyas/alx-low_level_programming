f773530fe83489118e37f78d2198882b80063118
sumeya <sumeyaskr@gmail.com>
2022-07-07 06:02:19 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - description
+ * 
+ */
+
+void print_numbers(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
