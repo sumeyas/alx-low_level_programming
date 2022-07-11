52b9ed8b4f7941c9da40c77a437c043128963c62
sumeya <sumeyaskr@gmail.com>
2022-07-11 12:46:41 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * rev_string - description
+ * @s: parameter
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int len;
+	char *st;
+	char *end;
+	char temp;
+
+	len = _strlen(s);
+	st = s;
+	end = s;
+
+	for (i = 0; i < len - 1; i++)
+	{
+		end++;
+	}
+	for (i =0; i < len / 2; i++)
+	{
+		temp = *end;
+		*end = *st;
+		*st = temp;
+
+		st++;
+		end--
+	}
+}
+	
+
+
+
+
