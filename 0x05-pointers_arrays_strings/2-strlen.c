5567aac40553475fb03384432eee1c01fb23236c
sumeya <sumeyaskr@gmail.com>
2022-07-11 10:42:40 -0700

    second queation

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - description
+ * @s: param
+ * Return: count
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		p++;
+	}
+	return (count);
+}
