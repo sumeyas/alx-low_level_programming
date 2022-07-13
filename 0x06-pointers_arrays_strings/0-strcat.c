02d71948dcab8dc69a3cd5bfc3e9043154b15911
sumeya <sumeyaskr@gmail.com>
2022-07-13 10:21:05 -0700

    message

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - description
+ * *_strcat - description
  * @dest: param
  * @src: param
+ * Return: value
  */
 char *_strcat(char *dest, char *src)
 {
@@ -11,18 +12,20 @@ char *_strcat(char *dest, char *src)
 	int len2 = 0;
 	int i;
 
-	while (dest != '\0')
+	while (*dest != '\0')
 	{
 		len1++;
+		dest++;
 	}
-	while (src != '\0')
+	while (*src != '\0')
 	{
 		len2++;
+		src++;
 	}
 	for (i = 0; i <= len2; i++)
 	{
 		dest[len1 + i] = src[i];
 	}
-	return dest;
+	return (dest);
 }
 
