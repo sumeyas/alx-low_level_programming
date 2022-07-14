8ef025c112fbf6de83933d5de96889b4bf5e4754
sumeya <sumeyaskr@gmail.com>
2022-07-14 12:11:16 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - desr
+ * @s: param
+ * Return: output\/
+ */
+
+char *leet(char *s)
+{
+	char arr[] = "a4A4e3E3o0O0t7T7l1L1";
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; j++)
+	{
+		for (j = 0; arr[j] != '\0'; j++)
+		{
+			if (s[i] == arr[j])
+			{
+				s[i] = arr[j + 1];
+				break;
+			}
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ int _strcmp(char *s1, char *s2);
 void reverse_Array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
+char *leet(char *s);
